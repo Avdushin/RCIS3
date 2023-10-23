379b45551f85b3d636ff783776bff40ded5c31c8
Avdushin <avdushinbeaver1@gmail.com>
2023-10-23 16:19:26 +0300

    Init LAB

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LrPr3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtNumber.Location = new System.Drawing.Point(155, 162);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(286, 31);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.resultTextBox.AutoSize = true;
+            this.resultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.resultTextBox.Location = new System.Drawing.Point(150, 99);
+            this.resultTextBox.Margin = new System.Windows.Forms.Padding(23, 0, 23, 0);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(0, 26);
+            this.resultTextBox.TabIndex = 1;
+            this.resultTextBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.resultTextBox.Click += new System.EventHandler(this.resultTextBox_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalculate.Location = new System.Drawing.Point(221, 213);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(143, 36);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 366);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.txtNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label resultTextBox;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LrPr3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Вывод квадратов натуральных чисел = ДКИП-404 Авдюшин";
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnCalculate_Click_1(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNumber.Text, out int number))
+            {
+                int square = number * number;
+                resultTextBox.Text = $"Квадрат числа {number} равен {square}";
+            }
+            else
+            {
+                resultTextBox.Text = "Пожалуйста, введите корректное натуральное число.";
+            }
+        }
+
+        private void resultTextBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
